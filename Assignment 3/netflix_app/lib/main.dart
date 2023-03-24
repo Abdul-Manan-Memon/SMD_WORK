e9b657324543b67707f2834fb2efc4ecb1132bae
Abdul Manan <k191071@nu.edu.pk>
2023-03-24 23:40:28 +0500

    Assignment 3 Done

---
+++ Assignment 3/netflix_app/lib/main.dart
@@ -0,0 +1,15 @@
+// Create By -> {
+//Sanjay kumar (19k-1052),
+//Gul Muneer (19k-1384),
+// Abdul Manan (19k-1071)
+//}
+import 'package:flutter/material.dart';
+import '/pages/root_app.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+    home: RootApp(),
+  ));
+}
